75a7425026359a438b2c6bc09e02576a3530cf71
Noah Tye <hi@noahtye.com>
2024-08-16 21:42:57 -0700

    use css.raw for shared styles

--- app/styles.ts
+++ app/styles.ts
@@ -1,5 +1,7 @@
-export const sourceCodeStyle = {
+import { css } from "@/styled-system/css";
+
+export const sourceCodeStyle = css.raw({
   fontSize: "0.9em",
   fontFamily: "'Source Code Pro', monospace",
   whiteSpace: "pre-wrap",
-};
+});
